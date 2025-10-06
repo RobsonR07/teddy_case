ec033ccd0bb9003d106bf09f8904ec89d9a9bcbb
Robson Ribeiro <robson_lucio98@hotmail.com>
2025-10-05 23:41:38 -0300

    fix: padroniza nomes de colunas para minúsculas no schema e DataFrame

--- src/data_cleaner.py
+++ src/data_cleaner.py
@@ -5,7 +5,9 @@ def filter_completed_records(df: pd.DataFrame) -> pd.DataFrame:
     initial_count = len(df)
     df_filtered = df[df["completed"] == True].copy()
     final_count = len(df_filtered)
+    df_filtered = df_filtered.rename(columns={"userId": "user_id"})
+
 
     removed = initial_count - final_count
-    print(f"Foram removidos {removed} registros incmpletos.")
+    print(f"Foram removidos {removed} registros incompletos.")
     return df_filtered
